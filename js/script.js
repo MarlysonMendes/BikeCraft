ca621cd9a68f89748112c34d14e93569ece6fec9
Marlyson dos Santos Mendes <50031055+MarlysonMendes@users.noreply.github.com>
2021-01-21 21:45:13 -0300

    plugin

---
+++ js/script.js
@@ -0,0 +1,5 @@
+new SimpleSlide({
+    slide: 'quote', // nome do atributo data-slide="principal"
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides
+  });
