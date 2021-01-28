d36f74f68f6dde7e7c31708c2946021801817199
Marlyson dos Santos Mendes <50031055+MarlysonMendes@users.noreply.github.com>
2021-01-27 23:22:10 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-new SimpleSlide({
+if(window.SimpleSlide){
+  new SimpleSlide({
     slide: 'quote', // nome do atributo data-slide="principal"
     auto: true, // se o slide deve passar automaticamente
     time: 5000, // tempo de transição dos slides
   });
+
+  new SimpleSlide({
+    slide: 'portifolio',
+    auto: true, 
+    time: 5000, 
+    nav: true
+  });
+}
+
+if(window.SimpleAnime){
+  new SimpleAnime();
+}
